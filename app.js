6616c44e27907b7d248b1327783a878c4ef55cba
Rea <dilemajire@gmail.com>
2021-02-01 19:52:41 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const {crearArchivo} = require('./helpers/multiplicar');
+//const colors = require('colors');//-> Si ocupas acceder a más configs
+const argv = require('./config/yargs');
+
+require('colors');
+console.clear();
+
+
+
+// const [ , , arg3='base=5'] = process.argv;
+//
+// const [ , base = 5] = arg3.split('=');
+
+//console.log(argv);
+//console.log('base: yargs ', argv.b);
+
+
+
+//const base = 2;
+
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then((nombreArchivo) => console.log(nombreArchivo.rainbow, ' creado'))
+    .catch((err) => console.log(err));
